e5e73afc9a15b4723a5a9454f223512f5497f477
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-05-31 20:19:21 +0200

    drobna zmiana

--- blog/views.py
+++ blog/views.py
@@ -40,8 +40,6 @@ def post_edit(request, pk):
     return render(request, 'blog/post_edit.html', {'form': form})
 
 def post_delete(request, pk):
-    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     post = Post.objects.get(pk=pk)
     post.delete()
     return redirect('post_list')
-    #return render(request, 'blog/post_list.html', {'posts':posts})
