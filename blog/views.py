b9f17e1f8038dfc35ef05be4bf77ace621d1fc5d
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-05-31 20:18:44 +0200

    dodano delete

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
 
 def post_detail(request, pk):
@@ -38,3 +38,10 @@ def post_edit(request, pk):
     else:
         form = PostForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_delete(request, pk):
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    post = Post.objects.get(pk=pk)
+    post.delete()
+    return redirect('post_list')
+    #return render(request, 'blog/post_list.html', {'posts':posts})
