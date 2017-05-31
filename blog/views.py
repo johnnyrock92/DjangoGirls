f4c589a85dc2e2234a8891a7b4d16f98f8be8b9a
Janusz Schönrock <janusz.schonrock@outlook.com>
2017-05-31 22:52:27 +0200

    dodawanie obrazkow

--- blog/views.py
+++ blog/views.py
@@ -14,11 +14,12 @@ def post_detail(request, pk):
 
 def post_new(request):
     if request.method == "POST":
-        form = PostForm(request.POST)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.author = request.user
             post.published_date = timezone.now()
+            post.image = form.cleaned_data['image']
             post.save()
             return redirect('post_detail', pk=post.pk)
     else:
--- mysite/settings.py
+++ mysite/settings.py
@@ -124,3 +124,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'public', 'static')
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
